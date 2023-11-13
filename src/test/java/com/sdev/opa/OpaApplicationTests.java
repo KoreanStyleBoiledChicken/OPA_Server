fa54041220658b182d996d6b63b2c1289629b4fb
잉퓨 <dayeon620@kakao.com>
2023-11-13 13:07:42 +0900

    project init

---
+++ src/test/java/com/sdev/opa/OpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdev.opa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
